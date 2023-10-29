b79c0e824852703bed96142495717b7b20964db9
Delanoy <delanoy@Delanoys-MacBook-Pro.local>
2023-10-29 08:07:39 +0530

    addresses db

--- addressesDb.js
+++ addressesDb.js
@@ -3,8 +3,8 @@ const addresses = [
         "id": 1,
         "name": "Delanoy Kunil",
         "walletHash": "0x0688bd1b30b41ff244094b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040968451,
+        "lastInteracted": 1694940179240,
         "verified": true,
         "groups": [
             {"groupId": 3,},
@@ -17,7 +17,7 @@ const addresses = [
         "id": 2,
         "name": "Zhao Yan",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040839468,
         "verified": true,
         "groups": []
     },
@@ -25,8 +25,8 @@ const addresses = [
         "id": 3,
         "name": "Vladmir Putin",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040592837,
+        "lastInteracted": 1694940651938,
         "verified": true,
         "groups": [
             {"groupId": 4,},
@@ -37,7 +37,7 @@ const addresses = [
         "id": 4,
         "name": "Christy Walton",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040509294,
         "verified": false,
         "groups": [
             {"groupId": 2,}
@@ -47,8 +47,8 @@ const addresses = [
         "id": 5,
         "name": "Diane Hendricks",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040495062,
+        "lastInteracted": 1694940693284,
         "verified": false,
         "groups": []
     },
@@ -56,8 +56,8 @@ const addresses = [
         "id": 6,
         "name": "Blair Parry-Okeden",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040497028,
+        "lastInteracted": 1694940793284,
         "verified": true,
         "groups": [
             {"groupId": 1,}
@@ -70,7 +70,7 @@ const addresses = [
         "id": 7,
         "name": "Nancy Walton Laurie",
         "walletHash": "0x0688bd1b30b41ff244094b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040423910,
         "lastInteracted": 1694940359891,
         "verified": false,
         "groups": [
@@ -82,8 +82,8 @@ const addresses = [
         "id": 8,
         "name": "Zeng Fangquin",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040642912,
+        "lastInteracted": 1694940592385,
         "verified": true,
         "groups": [
             {"groupId": 1,}
@@ -93,7 +93,7 @@ const addresses = [
         "id": 9,
         "name": "Cheng Xue",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040349682,
         "verified": false,
         "groups": [
             {"groupId": 4,}
@@ -103,7 +103,7 @@ const addresses = [
         "id": 10,
         "name": "Maria",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040458612,
         "verified": true,
         "groups": [
             {"groupId": 1,}
@@ -113,8 +113,8 @@ const addresses = [
         "id": 11,
         "name": "Ann Walton Kroenke",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040493268,
+        "lastInteracted": 1694940573914,
         "verified": true,
         "groups": [
             {"groupId": 2,}
@@ -124,8 +124,8 @@ const addresses = [
         "id": 12,
         "name": "Denise Coates",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040684295,
+        "lastInteracted": 1694940579284,
         "verified": false,
         "groups": [
             {"groupId": 4,}
@@ -137,8 +137,8 @@ const addresses = [
         "id": 13,
         "name": "Delanoy Kunil",
         "walletHash": "0x0688bd1b30b41ff244094b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040582936,
+        "lastInteracted": 1694940683916,
         "verified": true,
         "groups": [
             {"groupId": 3,},
@@ -149,8 +149,8 @@ const addresses = [
         "id": 14,
         "name": "Massimiliana Landini Alleotti",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040384759,
+        "lastInteracted": 1694940659283,
         "verified": true,
         "groups": [
             {"groupId": 2,}
@@ -160,8 +160,8 @@ const addresses = [
         "id": 15,
         "name": "Kwong Siu-hing",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040583976,
+        "lastInteracted": 1694940439729,
         "verified": true,
         "groups": [
             {"groupId": 4,}
@@ -171,8 +171,8 @@ const addresses = [
         "id": 16,
         "name": "Wang Laichun",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040583765,
+        "lastInteracted": 1694940481974,
         "verified": true,
         "groups": [
             {"groupId": 2,}
@@ -182,7 +182,7 @@ const addresses = [
         "id": 17,
         "name": "Pablo Escobar",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
+        "created": 1694040485823,
         "verified": false,
         "groups": [
             {"groupId": 3,}
@@ -192,8 +192,8 @@ const addresses = [
         "id": 18,
         "name": "Kirsten Rausing",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040381976,
+        "lastInteracted": 1694940421976,
         "verified": true,
         "groups": [
             {"groupId": 1,}
@@ -203,8 +203,8 @@ const addresses = [
         "id": 19,
         "name": "Abigail Johnson",
         "walletHash": "0x0688bd1b30b41ff244094b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040378175,
+        "lastInteracted": 1694940401982,
         "verified": true,
         "groups": [
             {"groupId": 3,},
@@ -215,8 +215,8 @@ const addresses = [
         "id": 20,
         "name": "Charlene de Carvalho-Heineken",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040587162,
+        "lastInteracted": 1694940471825,
         "verified": true,
         "groups": [
             {"groupId": 4,}
@@ -226,8 +226,8 @@ const addresses = [
         "id": 21,
         "name": "Vladmir Putin",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040498148,
+        "lastInteracted": 1694946183965,
         "verified": true,
         "groups": [
             {"groupId": 1,}
@@ -237,8 +237,8 @@ const addresses = [
         "id": 22,
         "name": "Iris Fontbona ",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040471846,
+        "lastInteracted": 1694940748391,
         "verified": false,
         "groups": [
             {"groupId": 2,}
@@ -248,8 +248,8 @@ const addresses = [
         "id": 23,
         "name": "Gina Rinehart",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040482936,
+        "lastInteracted": 1694940528392,
         "verified": true,
         "groups": [
             {"groupId": 4,}
@@ -259,8 +259,8 @@ const addresses = [
         "id": 24,
         "name": "Lu Zhongfang",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040458172,
+        "lastInteracted": 1694940398461,
         "verified": true,
         "groups": [
             {"groupId": 4,}
@@ -271,8 +271,8 @@ const addresses = [
         "id": 25,
         "name": "Zhong Huijuan",
         "walletHash": "0x0688bd1b30b41ff244094b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040349581,
+        "lastInteracted": 1694940304918,
         "verified": false,
         "groups": [
             {"groupId": 2,},
@@ -283,8 +283,8 @@ const addresses = [
         "id": 26,
         "name": "Laurene Powell Jobs",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040495728,
+        "lastInteracted": 1694940492864,
         "verified": false,
         "groups": [
             {"groupId": 3,}
@@ -294,8 +294,8 @@ const addresses = [
         "id": 27,
         "name": "Susanne Klatten",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040441936,
+        "lastInteracted": 1694940410938,
         "verified": true,
         "groups": [
             {"groupId": 4,}
@@ -305,8 +305,8 @@ const addresses = [
         "id": 28,
         "name": "Jacqueline Mars",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040235967,
+        "lastInteracted": 1694940320978,
         "verified": false,
         "groups": [
             {"groupId": 1,}
@@ -316,8 +316,8 @@ const addresses = [
         "id": 29,
         "name": "Yang Huiyan",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 1694040302984,
+        "lastInteracted": 1694940387510,
         "verified": false,
         "groups": [
             {"groupId": 2,}
@@ -327,8 +327,8 @@ const addresses = [
         "id": 30,
         "name": "Julia Koch",
         "walletHash": "0x0688bd1b30b41ff24409454ad7068b5da459e2ed",
-        "created": 1694040359891,
-        "lastInteracted": 1694940359891,
+        "created": 16940404019274,
+        "lastInteracted": 1694940413985,
         "verified": false,
         "groups": [
             {"groupId": 1,}
